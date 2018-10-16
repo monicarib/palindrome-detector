41427d73eb4f8e6ade892a8003432c5406162cd7
Mônica Ribeiro <monicarib1@gmail.com>
2018-10-16 15:47:14 -0300

    Declaring unit tests.

---
+++ src/test/java/palindromedetector/palindromedetector/PalindromeDetectorTest.java
@@ -0,0 +1,29 @@
+package palindromedetector.palindromedetector;
+
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static org.junit.Assert.*;
+
+public class PalindromeDetectorTest {
+
+    @Test
+    public void isPalindrome_whenTextIsNotAPalidromeShouldReturnFalse() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void isPalindrome_whenTextIsPalindromeShouldReturnTrue() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void isPalindrome_whenTextIsNullShouldThrowException() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void isPalindrome_whenTextIsEmptyShouldThrowException() {
+        throw new NotImplementedException();
+    }
+}
