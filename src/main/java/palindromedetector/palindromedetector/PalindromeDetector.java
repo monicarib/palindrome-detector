5ca9bb4442c0547f6d543b704472ac276660333a
Mônica Ribeiro <monicarib1@gmail.com>
2018-10-16 15:50:18 -0300

    Implementing solution without tests.

--- src/main/java/palindromedetector/palindromedetector/PalindromeDetector.java
+++ src/main/java/palindromedetector/palindromedetector/PalindromeDetector.java
@@ -1,7 +1,15 @@
 package palindromedetector.palindromedetector;
 
 public class PalindromeDetector {
+
     public Boolean isPalindrome(String text) {
-        return false;
+        this.checkIfTextIsNull(text);
+        return text.equals(new StringBuilder(text).reverse().toString());
+    }
+
+    private void checkIfTextIsNull(String text) {
+        if(text == null) {
+            throw new NullPointerException("Text is null.");
+        }
     }
 }
