37f596d26723fc69fcdbb9ca64802b4fe2866444
Mônica Ribeiro <monicarib1@gmail.com>
2018-10-16 15:41:03 -0300

    First commit.

---
+++ src/main/java/palindromedetector/palindromedetector/PalindromeDetectorApplication.java
@@ -0,0 +1,12 @@
+package palindromedetector.palindromedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalindromeDetectorApplication.class, args);
+    }
+}
